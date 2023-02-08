0f5a64899aa1704c4175df4f2fc9fc4df9eac346
omri2084 <omri2084@cs.huji.ac.il>
2023-02-08 16:56:35 +0200

    updat bank

--- Bank/Bank.cs
+++ Bank/Bank.cs
@@ -29,20 +29,20 @@ public class Account
 public class Bank
 {
     private int numberOfAccount;
-    private IDictionary<int, Account> numberNames;
+    private IDictionary<string, Account> numberNames;
     public double interest;
     private double prevInterest;
     private bool isStartYear;
     public Bank()
     {
         this.numberOfAccount = 0;
-        this.numberNames = new Dictionary<int, Account>();
+        this.numberNames = new Dictionary<string, Account>();
         this.prevInterest = 0;
         this.interest = 0.3;
         this.isStartYear = false;
     }
 
-    public void addAccount(double moneyToAdd, int id)
+    public void addAccount(double moneyToAdd, string id)
     {
         if (this.numberNames.ContainsKey(id))
         {
@@ -53,7 +53,7 @@ public class Bank
         this.numberNames.Add(id, account);
         this.numberOfAccount++;
     }
-    public void addMoney(int id, double moneyToAdd)
+    public void addMoney(double moneyToAdd, string id)
     {
         if (!this.isStartYear)
         {
@@ -68,7 +68,7 @@ public class Bank
         this.numberNames[id].addMoney(moneyToAdd);
         Console.WriteLine("You have successfully added money");
     }
-    public void takeMoney(int id, double moneyToTake)
+    public void takeMoney(double moneyToTake, string id)
     {
         if (!this.isStartYear)
         {
@@ -84,7 +84,7 @@ public class Bank
         Console.WriteLine($"You have successfully take {t} from Your Account");
     }
 
-    public void deleteAccunt(int id)
+    public void deleteAccunt(string id)
     {
         if (!this.isStartYear)
         {
@@ -139,12 +139,69 @@ public class Bank
     {
         this.isStartYear = false;
     }
-
     void inStartOfYear()
     {
         //run this func in start of the year
         setStartOfYear();
         addToInterestAccount();
         updateInterest();
+    }
+    private string getID()
+    {
+        Console.WriteLine("enter your ID: ");
+        string id = Console.ReadLine();
+        return id;
+    }
+
+    private int getMoney()
+    {
+        Console.WriteLine("Enter the amount: ");
+        string money = Console.ReadLine();
+        int intMoney = Convert.ToInt32(money);
+        return intMoney;
+    }
+    void getDataOfAcc(string id)
+    {
+        Console.WriteLine(this.numberNames[id].getMoney());
+    }
+    public void runProgram()
+    {
+
+        this.setStartOfYear();
+        for (int i = 0; true; i++)
+        {
+            Console.WriteLine($"the year is {i}");
+            Console.WriteLine("Choose what action to take: ");
+            Console.WriteLine("1 - Add money to your account");
+            Console.WriteLine("2 - Withdraw money from your account");
+            Console.WriteLine("3 - Close your account");
+            Console.WriteLine("Enter the number that corresponds to the desired action.");
+            string coomend = Console.ReadLine();
+            int intCoomend = Convert.ToInt32(coomend);
+            string id;
+            int Money;
+            switch (intCoomend)
+            {
+                case 1:
+                    id = this.getID();
+                    Money = this.getMoney();
+                    this.addMoney(Money, id);
+                    break;
+                case 2:
+                    id = this.getID();
+                    Money = this.getMoney();
+                    this.takeMoney(Money, id);
+                    break;
+                case 3:
+                    id = this.getID();
+                    this.deleteAccunt(id);
+                    break;
+                default:
+                    Console.WriteLine("we move to the next year.");
+                    break;
+            }
+            this.inStartOfYear();
+            Console.WriteLine($"The number of accounts in the bank {this.numberOfAccount}");
+        }
     }
 }
