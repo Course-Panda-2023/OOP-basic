4d292d9ccd56bd9d6a40c5de69f4ae94e15a351f
razbroc <razbroc@gmail.com>
2023-02-09 01:15:09 +0200

    add(carGarage proj and car abstract class now)

---
+++ Cars/Car.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cars
+{
+    public abstract class Car
+    {
+        private int Price;
+        private int YearOfMake;
+        private int Km;
+        private string color;
+        private string manifactur;
+
+        protected Car(int price, int yearOfMake, int km, string color, string manifactur)
+        {
+            this.Price = price;
+            this.YearOfMake = yearOfMake;
+            this.Km = km;
+            this.color = color;
+            this.manifactur = manifactur;
+        }
+    }
+}
