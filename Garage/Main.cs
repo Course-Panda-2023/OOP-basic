1a72166ab6f5933702e09da17e3438687a82c9ae
ahmichal <95378632+ahmichal@users.noreply.github.com>
2023-02-08 18:30:46 +0200

    added main of assinment 3

--- Garage/Main.cs
+++ Garage/Main.cs
@@ -4,8 +4,12 @@ public class HelloWorld
 {
     public static void Main(string[] args)
     {
-        /*
-        Test your class here
-        */
+        GasCar car = new GasCar(50, 50, "green", 2013, "firmmmm");
+        ElectricCar car2 = new ElectricCar(50, 50, "green", 2013, "firmmmm");
+        CarsParkingLot list = new CarsParkingLot();
+        list.AddCar(car);
+        list.AddCar(car2);
+        list.PrintElectric();
+
     }
 }
