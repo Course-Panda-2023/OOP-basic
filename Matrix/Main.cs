490b379c21fa53cf182d98da6c95ab55f376381c
omri2084 <omri2084@cs.huji.ac.il>
2023-02-08 17:02:17 +0200

    matrix

--- Matrix/Main.cs
+++ Matrix/Main.cs
@@ -7,5 +7,10 @@ public class HelloWorld
         /*
         Test your class here
         */
+        Matrix m = new Matrix();
+        Matrix m1 = new Matrix(m);
+        Matrix m2 = m*4;
+        Matrix m3 = m +m2;
+        Matrix m4 = m -m3;
     }
 }
