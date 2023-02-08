94a58fd68a6e2bc09e2b790f6e9402c52e5b3ec9
xZoomie <71326632+xZoomie@users.noreply.github.com>
2023-02-09 01:04:49 +0200

    Revert "Bank + Garage"
    This reverts commit 0be59bde1e518f81aafe0966486e2c2ccb96e1ae.

---
+++ Bank/Main.cs
@@ -0,0 +1,12 @@
+using System;
+
+public class HelloWorld
+{
+    public static void Main(string[] args)
+    {
+       /*
+       The menu for the bank will be here
+       Make convenient and intuitive menu
+       */
+    }
+}
