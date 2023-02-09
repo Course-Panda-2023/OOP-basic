cc5d8305fd1aa0191b3bb4c5b71aff2dc972a0f8
NatalieShaked <shakednatalie@gmail.com>
2023-02-09 10:29:04 +0200

    started Matrix

--- Matrix/Matrix.cs
+++ Matrix/Matrix.cs
@@ -1,3 +1,14 @@
-/*
-Matrix class
-*/
+using System;
+
+public class Matrix
+{
+    private int[,] matrix;
+    public int GetRowAmount() { return matrix.GetLength(0); }
+    public void SetValueAt(int i, int j, int value) { matrix[i, j] = value; }
+
+    public Matrix(int[,] m)
+    {
+
+    }
+
+}
